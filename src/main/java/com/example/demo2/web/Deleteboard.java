c5edbfa20f4682848f686b0d27b4866bbeb5f6d4
TheNewLearn <sac05102634@gmail.com>
2022-06-08 06:03:04 +0800

    implement delete billboard

---
+++ src/main/java/com/example/demo2/web/Deleteboard.java
@@ -0,0 +1,19 @@
+package com.example.demo2.web;
+
+import com.example.demo2.repositories.BillboardRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+public class Deleteboard {
+    private BillboardRepository br;
+
+    @GetMapping(path="/delete")
+    public String remove(@RequestParam(name="id") Long id){
+        br.deleteById(id);
+        return "redirect:/index";
+    }
+}
--- src/main/java/com/example/demo2/web/Editboard.java
+++ src/main/java/com/example/demo2/web/Editboard.java
@@ -6,8 +6,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 @Controller
@@ -18,8 +22,17 @@ public class Editboard {
     public String editboard(Model model,@RequestParam(name="id") Long bid){
         Optional<Billboard> billboard = br.findById(bid);
         model.addAttribute("bordercontent",billboard.get());
-        System.out.println(billboard.get());
         return "board";
     }
+
+    @PostMapping(path="/border")
+    public String modifyborder(@RequestParam(name="id") Long id,@RequestParam(name="title") String title,@RequestParam(name="post_date") String post_date, @RequestParam(name="enddate") String enddate, @RequestParam(name="content") String content) throws Exception{
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date pd = formatter.parse(post_date);
+        Date ed = formatter.parse(enddate);
+        br.save(new Billboard(id,title,pd,"Admin",ed,content));
+        return "redirect:/index";
+    }
+
 
 }
